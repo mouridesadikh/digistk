2349c6c4ec8141f5edb3614569f1674fe72922d1
Cheikh Mouride <mouride987@gmail.com>
2019-06-11 18:47:16 +0000

    premier commit du projet gestion stock

---
+++ config/author.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company/Author/Developer/Licence information of the application
+    |--------------------------------------------------------------------------
+    |
+    | All details about the author/developer/contact/licence of the application
+    |
+    | IMPORTANT: CHANGING ANY OF THIS INFORMATION WILL UNSTABILIZE THE APPLICATION
+    |
+    */
+
+    'vendor' => 'DIGI221',
+
+    'vendor_url' => 'http://digi221.com',
+
+    'email' => 'thewebfosters@gmail.com',
+
+    'app_version' => "2.10",
+    'app_DIGI_personal_token' => 'ftXs1UtOgdtJFYo7ZcHHv46KH3PgLgch',
+    'app_DIGI_username' => 'thewebfosters',
+    'app_DIGI_product_code' => 21216332
+];
---
+++ readme.md
@@ -0,0 +1,15 @@
+## About Digi Stock
+
+Digi Stock is a POS application by [DIGI221](http://digi221.com), a brand of [The Web Fosters](http://thewebfosters.com).
+
+## Installation & Documentation
+You will find installation guide and documentation in the downloaded zip file.
+Also, For complete updated documentation of the Digi Stock please visit online [documentation guide](http://digi221.com/DStock/).
+
+## Security Vulnerabilities
+
+If you discover a security vulnerability within Digi Stock, please send an e-mail to support at thewebfosters@gmail.com. All security vulnerabilities will be promptly addressed.
+
+## License
+
+The Digi Stock software is licensed under the [digi221 license](https://digi221.net/licenses/standard).
